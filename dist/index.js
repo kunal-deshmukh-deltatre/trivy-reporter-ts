bb37e67d1f6342317f307b2d756dce5df9c863a6
Kunal Deshmukh <kunal.deshmukh@deltatre.com>
2022-12-12 16:32:08 +0530

    update

--- dist/index.js
+++ dist/index.js
@@ -37,18 +37,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(186));
 const exec = __importStar(__nccwpck_require__(514));
-const wait_1 = __nccwpck_require__(817);
 const fs = __importStar(__nccwpck_require__(747));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const ms = core.getInput('milliseconds');
+            const ms = core.getInput('endpointUrl');
             core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
             let files = yield exec.exec('ls');
             const content = yield fs.readFileSync('trivy_results.json', { encoding: 'utf-8' });
-            console.log(JSON.stringify(content));
-            yield (0, wait_1.wait)(parseInt(ms, 10));
-            core.debug(new Date().toTimeString());
+            const GIT_BRANCH = process.env.GITHUB_REF_NAME;
+            const GIT_COMMIT_ID = process.env.GITHUB_SHA;
+            // const PROJECT_NAME = tl.getVariable("System.TeamProject");
+            const GIT_REPOSITORY_URI = process.env.GITHUB_REPOSITORY;
+            // const PROJECT_ID = tl.getVariable("System.TeamProjectId");
+            const GIT_REPO_NAME = process.env.GITHUB_REPOSITORY;
+            const input = {
+                gitBranch: GIT_BRANCH,
+                gitCommitID: GIT_COMMIT_ID,
+                repositoryName: GIT_REPO_NAME,
+                gitRepositoryUrl: GIT_REPOSITORY_URI,
+                vulnerabilitiesFile: JSON.parse(content)
+            };
+            console.log(JSON.stringify(input));
             core.setOutput('time', new Date().toTimeString());
         }
         catch (error) {
@@ -58,37 +68,6 @@ function run() {
     });
 }
 run();
-
-
-/***/ }),
-
-/***/ 817:
-/***/ (function(__unused_webpack_module, exports) {
-
-"use strict";
-
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-function wait(milliseconds) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return new Promise(resolve => {
-            if (isNaN(milliseconds)) {
-                throw new Error('milliseconds not a number');
-            }
-            setTimeout(() => resolve('done!'), milliseconds);
-        });
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
