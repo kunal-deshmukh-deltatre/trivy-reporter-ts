2b559bb80473de81c3232ed7ea97650de51555ec
Kunal Deshmukh <kunal.deshmukh@deltatre.com>
2022-12-12 17:28:40 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import * as core from '@actions/core'
-import * as exec from '@actions/exec';
-import * as http from '@actions/http-client';
-import { wait } from './wait'
+import fetch from 'node-fetch';
 import * as fs from 'fs';
 async function run(): Promise<void> {
   try {
     const endpointUrl: string = core.getInput('endpointUrl')
     const apiKey: string = core.getInput('apiKey')
     core.debug(`Waiting ${endpointUrl} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-    console.log(endpointUrl, apiKey);    ;
+    console.log(endpointUrl, apiKey);;
     const content = await fs.readFileSync('trivy_results.json', { encoding: 'utf-8' });
     const GIT_BRANCH = process.env.GITHUB_REF_NAME
     const GIT_COMMIT_ID = process.env.GITHUB_SHA
@@ -27,9 +25,12 @@ async function run(): Promise<void> {
 
     }
     console.log(JSON.stringify(input));
-    const https = new http.HttpClient();
-    const ouput = await https.post(endpointUrl, input, { 'x-functions-key': apiKey });
-    core.setOutput('output', ouput)
+    const response = await fetch(endpointUrl, {
+      method: 'post',
+      body: input,
+      headers: { 'Content-Type': 'application/json', 'x-functions-key': apiKey }
+    });
+    core.setOutput('output', response)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
